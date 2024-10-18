7321950a246c6e63210eb6798c40603c95a48e78
Manuel <5673677+mtrezza@users.noreply.github.com>
2024-10-19 01:15:41 +0200

    ci: Fix auto release does not publish to npm (#112)

--- release.config.js
+++ release.config.js
@@ -87,7 +87,7 @@ async function config() {
         'changelogFile': changelogFile,
       }],
       ['@semantic-release/npm', {
-        'npmPublish': false,
+        'npmPublish': true,
       }],
       ['@semantic-release/git', {
         assets: [changelogFile, 'package.json', 'package-lock.json'],
